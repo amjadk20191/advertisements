19552be4968300ac5b1b994cc2b717184c0f9923
Amjad Kouzali <amjadkouzali2019@gmail.com>
2024-02-10 19:04:26 +0300

    fix

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY',"django-insecure-tii7tv&bg#nm-p&6^&-iasr3kncq6rt)dmsdjr%joun67j7*x4")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG',1)))
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS',"*")]
 
